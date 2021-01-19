22d1d72325d319135fab543c88bf634d620edc53
munozjuan <munoz.juan07@hotmail.com>
2021-01-18 23:27:08 -0500

    added gitignore and fired schema

--- EmployeeSchema.sql
+++ EmployeeSchema.sql
@@ -1,1 +1,19 @@
 DROP DATABASE IF EXISTS employee_managerDB;
+
+
+USE employee_managerDB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
