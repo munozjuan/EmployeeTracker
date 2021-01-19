4be933dc91bf32644f80387fbcd9a97a58e3957d
munozjuan <munoz.juan07@hotmail.com>
2021-01-19 17:14:34 -0500

    added function

--- app.js
+++ app.js
@@ -79,3 +79,49 @@ function runQuestions() {
         }
     });
 }
+
+function addEmployee() {
+    connection.query("SELECT title FROM employee_managerDB.role;", function(err, res) {
+        if (err) throw err;
+        let roleArr = [];
+        inquirer
+          .prompt([
+              {
+                  name: "role",
+                  type: "rawlist",
+                  choices: function() {
+                      for (let i = 0; i < res.length; i++) {
+                          roleArr.push(res[i].title);
+                      }
+                      return roleArr;
+                  },
+                  message: "What is the employee's role?"
+              },
+              {
+                  name: "firstname",
+                  type: "input",
+                  message: "What is the employee's first name?",
+              },
+              {
+                  name: "lastname",
+                  type: "input",
+                  message: "What is the employee's last name?"
+              },
+              {
+                  name: "manager",
+                  type: "number",
+                  message: "What is the employee's manager's ID#?"
+              }
+          ])
+          .then(function(answer) {
+              connection.query("INSERT INTO employee SET ?",
+              {
+                  first_name: answer.firstname,
+                  last_name: answer.lastname,
+                  role_id: roleArr.indexOf(answer.role)+1,
+                  manager_id: answer.manager
+              });
+              runQuestions();
+          });
+    });
+}
