880d26e6c94b101bb4175222982ee53f3002de1f
munozjuan <munoz.juan07@hotmail.com>
2021-01-19 17:16:01 -0500

    debugged app

--- app.js
+++ app.js
@@ -125,3 +125,33 @@ function addEmployee() {
           });
     });
 }
+
+
+function viewEmployee() {
+    let query = "SELECT DISTINCT emp1.id, concat(emp1.first_name, ' ', emp1.last_name) AS Employee, ro1.title AS Job_Title, ";
+    query += "dep1.name AS Department, ro1.salary, concat(man1.first_name, ' ', man1.last_name) AS Manager_Name FROM employee emp1 ";
+    query += "INNER JOIN role ro1 ON ro1.id = emp1.role_id INNER JOIN department dep1 ON ro1.department_id = dep1.id LEFT JOIN employee man1 ";
+    query += "ON emp1.manager_id = man1.id INNER JOIN employee emp2 ON ro1.id = emp2.role_id ORDER BY id";
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        runQuestions();
+    });
+}
+
+function addDepartment() {
+inquirer
+    .prompt(
+        {
+            name: "dept",
+            type: "input",
+            message: "What is the name of the new department you'd like to add?"
+        }
+    ).then(function(answer) {
+        let query = "INSERT INTO department SET ?"
+        connection.query(query, { name: answer.dept }, function(err) {
+            if (err) throw err;
+            runQuestions();
+        });
+    });
+}
